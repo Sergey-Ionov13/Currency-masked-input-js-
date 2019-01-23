9fbdc359c7c850e6985956b809bdcd35f7927bdd
Sergey <s.dyadya13@gmail.com>
2019-01-23 22:44:21 +0200

    Refactor all functions

--- currencyMaskedInput.js
+++ currencyMaskedInput.js
@@ -4,22 +4,14 @@ currencyMaskedInput();
 //функция, форматирующая ввод данных пользователя к денежному формату 00,000,000
 function currencyMaskedInput() {
 
-    //получение массива полей input с атрибутом currency
-    let inputFields = document.querySelectorAll("input[currency]");
-    let arrInputs = [];
-    for (let i = 0; i < inputFields.length; i++) {
-        arrInputs.push(inputFields[i]);
-    }
+  //получение списка полей input с атрибутом currency
+  let inputFields = document.querySelectorAll("input[currency]");
 
-    //замена введённого в поле текста на значение необходимого формата
-    for (let i = 0; i < arrInputs.length; i++) {
-        arrInputs[i].addEventListener("keyup", function () {
-            const inputValue = arrInputs[i]['value'];
-            if (inputValue) {
-                arrInputs[i]['value'] = currencyMask(inputValue);
-            }
-        });
-    }
+  //замена введённого в поле текста на значение необходимого формата
+  inputFields.forEach((elem) => elem.addEventListener('input', function () {
+    const inputValue = elem['value'];
+    elem['value'] = inputValue ? currencyMask(inputValue) : null;
+  }));
 }
 
 
@@ -32,50 +24,49 @@ function currencyMaskedInput() {
 3. Иначе возвращаем только преобразованный к строке arrayFromString.
  */
 function currencyMask(str) {
-    const myReg = /(\d+((,\d+)+)?\.?)/g;
-    //проверка на число вводимых пользователем данных
-    if (str.search(myReg) === -1) {
-        return '';
-    }
-    const regRest = /\.(\d+)?/g;
+  const myReg = /(\d+((,\d+)+)?\.?)/g;
+  //проверка на число вводимых пользователем данных
+  if (str.search(myReg) === -1) {
+    return '';
+  }
+  const regRest = /\.(\d+)?/g;
 
-    const arrayFromString = getSubStr(str, myReg).split('');
-    const mArray = maskedArray(arrayFromString);
+  const arrayFromString = getSubStr(str, myReg).split('');
+  const mArray = maskedArray(arrayFromString);
 
-    if (arrayFromString[arrayFromString.length - 1] === '.') {
-        const strRest = getSubStr(str, regRest);
-        return mArray.join('') + strRest;
-    }
+  if (arrayFromString[arrayFromString.length - 1] === '.') {
+    const strRest = getSubStr(str, regRest);
+    return mArray.join('') + strRest;
+  }
 
-    return mArray.join('');
+  return mArray.join('');
 }
+
+
 
 //получение подстроки, соответствующей регэкспу
 function getSubStr(str, regularExp) {
-    return str.match(regularExp)[0];
+  return str.match(regularExp)[0];
 }
+
+
 
 //формат массива, состоящего из символов строки
 function maskedArray(arr) {
+  //получаем очищенный от "," и "." массив, если они присутствуют
+  let clearArray = cleaningArray(arr);
+  //вставляем разделитель "," через каждые 3 элемента массива (справа налево)
+  let capacity = clearArray.length - 3;
+  while (capacity > 0) {
+    clearArray.splice(capacity, 0, ',');
+    capacity -= 3;
+  }
+  return clearArray;
+}
 
-    //получаем очищенный от "," массив, если они присутствуют
-    let arrWithoutComma = deleteAllCommasFromArray(arr);
 
-    //присваиваем переменной mArray массив без разделителя дробной части
-    let mArray = (arrWithoutComma[arrWithoutComma.length - 1] === '.') ? arrWithoutComma.slice(0, arrWithoutComma.length - 1) : arrWithoutComma.slice();
-    //вставляем разделитель "," через каждые 3 элемента массива (справа налево)
-    let capacity = mArray.length - 3;
-    while (capacity > 0) {
-        mArray.splice(capacity, 0, ',');
-        capacity -= 3;
-    }
 
-    //фильтрация массива ( получаем массив без ",")
-    function deleteAllCommasFromArray(arr) {
-        return  arr.filter(function (elem) {
-            return elem !== ',';
-        });
-    }
-
-    return mArray;
+//фильтрация массива ( получаем массив без "," и без ".")
+function cleaningArray(arr) {
+  return  arr.filter( (elem) => elem !== ',' && elem !== '.');
 }
